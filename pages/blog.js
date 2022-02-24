aa698b8242befda6e16e04fd7bf0d79261173130
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-02-24 12:07:53 +0600

    Next js server side authentication using getSession

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { getSession } from "next-auth/react";
+import axios from "axios";
 
-const Blog = () => {
+const Blog = ({ posts }) => {
+  console.log("posts data ::", posts);
+
   return (
-    <div className="w-75 mx-auto">Blog page</div>
-  )
-}
+    <div>
+      {posts ? (
+        <div>
+          {posts.map((post) => (
+            <div className="my-2 py-2 px-4 border w-75 border-info mx-auto rounded">
+              <h4 className="text-capitalize">{post.title}</h4>
+              <hp>{post.body}</hp>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="my-2 py-2 px-4 border w-75 border-info mx-auto rounded">
+          <h4 className="text-capitalize">Show Blog Post Must be a logged in user</h4>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export const getServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  let res = null;
+
+  if (session) {
+    res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+  }
+
+  return { props: { posts: res ? res.data : res } };
+};
 
 export default Blog;
